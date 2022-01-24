69a1cd8d44974259bff727a3f0ce0cbe254fb079
Zyrakia <mailzyrakia@gmail.com>
2022-01-24 09:21:06 -0800

    fix: exported minmax player and test output

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,12 @@ export { TicTacToeMoveResult } from './enum/TicTacToeMoveResult';
 
 export { TicTacToeOutput } from './output/TicTacToeOutput';
 export { TicTacToeStringOutput } from './output/TicTacToeStringOutput';
+export { TicTacToeTestOutput } from './output/TicTacToeTestOutput';
 
 export { TicTacToePlayer } from './player/TicTacToePlayer';
 export { TicTacToeRandomPlayer } from './player/TicTacToeRandomPlayer';
 export { TicTacToeManualPlayer } from './player/TicTacToeManualPlayer';
+export { TicTacToeMinMaxPlayer } from './player/TicTacToeMinMaxPlayer';
 
 export { TicTacToeBoard } from './TicTacToeBoard';
 export { TicTacToeGame } from './TicTacToeGame';
